d3fb6d27173398f9a5d61094357c96364fcd2830
francisco8628 <francisco.ableite@gmail.com>
2021-11-11 17:21:19 -0300

    spring web mvc com ModelAndViel

--- src/main/java/com/webMvc/springwemvc/contoller/JediController.java
+++ src/main/java/com/webMvc/springwemvc/contoller/JediController.java
@@ -1,15 +1,27 @@
 package com.webMvc.springwemvc.contoller;
 
+import java.util.List;
+
+import com.webMvc.springwemvc.model.Jedi;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class JediController {
    
 	@GetMapping("/jedi")
-	public String jedi() {
+	public ModelAndView jedi() {
 		
-		return "jedi";  //jedi vai buscar o mesmo arquivo html
+		final ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("jedi");   //o nome da url vai ser jedi
+        final Jedi luke = new Jedi("luke", "sky");
+		modelAndView.addObject("allJedi", List.of(luke));
+		
+		
+		
+		return modelAndView;  //jedi vai buscar o mesmo arquivo html
 	}
+	
 	
 }
